b1817d3ca01f5d6663f2a70fb7d929a9fd481126
EmilianoChavezDev <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-02-24 13:07:57 -0300

    Scrum-8-ImplementacionRegistro
    Scrum-8-Implementacion de logica para el registro de usuario

--- src/app/(Auth)/Registrarse/page.js
+++ src/app/(Auth)/Registrarse/page.js
@@ -1,8 +1,42 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import styles from "./styles/Registrarse.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import useAuth from "@/hooks/useAuth";
+import { useUser } from "@/contexts/UserProvider";
+import moment from "moment";
+
+const defaultValues = {
+  username: "",
+  password: "",
+  password_confirmation: "",
+};
+
 const page = () => {
+  const { data, error, loading, register: registro } = useAuth();
+  const { login: saveUser } = useUser();
+
+  useEffect(() => {
+    if (!data || error) return;
+    saveUser(data);
+  }, [data]);
+
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm({ defaultValues });
+
+  const onSubmit = async (formData) => {
+    formData.role = "usuario";
+    const fecha = moment(formData.fecha_nacimiento).format("DD-MM-YYYY");
+    formData.fecha_nacimiento = fecha;
+    registro(formData);
+  };
+
   return (
     <div className={styles.content}>
       <div className={styles.content_image}>
@@ -13,33 +47,64 @@ const page = () => {
           <div className={styles.content_informacion}>
             <div className={styles.content_title_correo}>
               <p>Nombre de usuario</p>
-              <input type="text" placeholder="nickname" />
+              <input
+                type="text"
+                placeholder="nickname"
+                {...register("username", {
+                  required: "Debe ingresar su nombre de usuario",
+                })}
+                onBlur={() => trigger("username")}
+              />
             </div>
-            <div>
-              <p>Correo electrónico</p>
-              <input type="email" placeholder="example@email.com" />
-            </div>
+
             <div>
               <p>Nueva contraseña</p>
-              <input type="password" placeholder="contraseña nueva" />
+              <input
+                type="password"
+                placeholder="ej: pass1234"
+                {...register("password", {
+                  required: "Debe ingresar su contraseña",
+                })}
+                onBlur={() => trigger("password")}
+              />
             </div>
             <div>
               <p>Confirmar la contraseña</p>
-              <input type="password" placeholder="confirmar contraseña" />
+              <input
+                type="password"
+                placeholder="repita la contraseña"
+                {...register("password_confirmation", {
+                  required: "Debe ingresar su contraseña",
+                })}
+                onBlur={() => trigger("password_confirmation")}
+              />
             </div>
             <div className={styles.content_date}>
               <p>Fecha de nacimiento</p>
-              <input type="date" />
+              <input
+                type="date"
+                {...register("fecha_nacimiento", {
+                  required: "Debe ingresar su contraseña",
+                })}
+                onBlur={() => trigger("fecha_nacimiento")}
+              />
             </div>
           </div>
           <div className={styles.content_button_submit}>
-            <button type="submit">Registrarte</button>
+            <button
+              type="submit"
+              id="register-btn"
+              onClick={handleSubmit(onSubmit)}
+              disabled={loading}
+            >
+              {loading ? "Registrandosé..." : "Registrarte"}
+            </button>
           </div>
 
           <div />
           <div className={styles.content_button}>
             <p>¿Tienes una cuenta?</p>
-            <Link href={"/Login"}>Iniciar Sesion</Link>
+            <Link href={"/auth/login "}>Iniciar Sesion</Link>
           </div>
         </div>
       </div>
