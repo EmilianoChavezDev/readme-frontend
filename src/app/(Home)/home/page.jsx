b939a965213d353c48f6e935d386bc2b437466c5
HamudiEz <hamudi231001@gmail.com>
2024-02-24 19:06:46 -0300

    SCRUM-10 Front sin back

---
+++ src/app/(Home)/home/page.jsx
@@ -0,0 +1,77 @@
+"use client";
+
+import {
+  Avatar,
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+
+export default function BackgroundBlogCard() {
+  return (
+    <div className="container-fluid px-2 py-12 pb-28 m-28">
+      <div className="mb-32">
+        <h2 className="text-5xl font-semibold mb-4">Seguir Leyendo</h2>
+
+        <div className="flex gap-8 overflow-x-auto">
+          {[...Array(5)].map((_, index) => (
+            <a
+              key={index}
+              href={`/article/${index}`}
+              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+            >
+              <Card
+                shadow={false}
+                className="relative grid h-[40rem] w-full max-w-[28rem] items-end justify-center overflow-hidden text-center"
+              >
+                <CardHeader
+                  floated={false}
+                  shadow={false}
+                  color="transparent"
+                  className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-center"
+                >
+                  <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
+                </CardHeader>
+                <CardBody className="relative py-8 px-6 md:px-8">
+                  
+                </CardBody>
+              </Card>
+            </a>
+          ))}
+        </div>
+      </div>
+      {/* Línea de sombreado */}
+      <div className="border-t-2 border-gray-200 my-12"></div>
+      <div>
+        <h2 className="text-5xl font-semibold mb-4 ">Te puede interesar</h2>
+        <div className="flex gap-8 overflow-x-auto">
+          {[...Array(5)].map((_, index) => (
+            <a
+              key={index + 5}
+              href={`/article/${index + 5}`}
+              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+            >
+              <Card
+                shadow={false}
+                className="relative grid h-[40rem] w-full max-w-[28rem] items-end justify-center overflow-hidden text-center"
+              >
+                <CardHeader
+                  floated={false}
+                  shadow={false}
+                  color="transparent"
+                  className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-center"
+                >
+                  <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
+                </CardHeader>
+                <CardBody className="relative py-8 px-6 md:px-8">
+                 
+                </CardBody>
+              </Card>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
