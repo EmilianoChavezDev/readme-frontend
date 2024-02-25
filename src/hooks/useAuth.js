d2fc8ba98570c86badd14880a16e26631b6da956
EmilianoChavezDev <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-02-25 18:10:26 -0300

    Scrum-8-ImplementacionRegistro
    Scrum-8-Resolviendo conflictos

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import axios from "axios";
 
@@ -51,39 +50,6 @@ const useAuth = () => {
   };
 
   return { data, loading, error, login, register };
-
-import { useState } from 'react';
-import axios from 'axios';
-
-const useAuth = () => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false)
-
-
-    const login = async (body) => {
-        setLoading(true)
-        try {
-            const response = await axios.post(`${process.env.API_URL}/login`, body);
-
-            if(response.status <200 || response.status>=300){
-                throw error;
-            }
-
-           const data =  await response.data
-           setData(data)
-           setError(false)
-        } catch (error) {
-            setError(true)
-            setLoading(false)
-        } finally {
-            setLoading(false)
-        }
-    };
-
-
-    return { data, loading, error, login };
-
 };
 
 export default useAuth;
