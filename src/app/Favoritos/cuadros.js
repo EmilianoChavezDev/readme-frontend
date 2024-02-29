f182f725fab5216a48097ee90c8a536f26b43ca8
Jorge <jorgemf195@gmail.com>
2024-02-28 22:38:03 -0300

    separando en clases diferentes cuadros y page

--- src/app/Favoritos/cuadros.js
+++ src/app/Favoritos/cuadros.js
@@ -1,36 +1,38 @@
-import { useClient } from 'next/client';
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./styles/favoritos.module.css";
 
-
-const cuadros = (key, imageurl, title, author) => {
-  const [favorito, setFavorito] = useState(false);
+const cuadros = (id, imageurl, title, author) => {
+  const [favorito, setFavorito] = useState(true);
 
   const fn_btnFavorite = () => {
     setFavorito(!favorito);
   };
-  
+
   return (
-    <>
-      <div key={key} className={styles.contenedor_datos_cuadro}>
-        <div className={styles.botonContainer} onClick={fn_btnFavorite}>
+    <div key={id} className={styles.contenedor_datos_cuadro}>
+      <div className={styles.botonContainer} onClick={fn_btnFavorite}>
+        {favorito ? (
           <button className={styles.btnCorazonLleno}>
             <img src="/image/corazon_lleno.png" alt="Corazón lleno" />
           </button>
+        ) : (
           <button className={styles.btnCorazonVacio} onClick={fn_btnFavorite}>
             <img src="/image/corazon_vacio.png" alt="Corazón vacio" />
           </button>
-        </div>
-        <div>
-          <img src={imageurl}></img>
-        </div>
-        <div>
-          <p className={styles.title}>{title}</p>
-        </div>
-        <div>
-          <p className={styles.author}>{author}</p>
-        </div>
+        )}
       </div>
-    </>
+      <div>
+        <img src={imageurl}></img>
+      </div>
+      <div>
+        <p className={styles.title}>{title}</p>
+      </div>
+      <div>
+        <p className={styles.author}>{author}</p>
+      </div>
+    </div>
   );
 };
 
