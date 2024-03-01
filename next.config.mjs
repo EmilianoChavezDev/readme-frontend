da474ada2c602471fb024d39daf677b12f1c85f3
godoygabriel72 <godoygabriel72@gmail.com>
2024-03-01 19:45:14 -0300

    Agregado cambios sugeridos por Emiliano

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [     // TODO: remove at ticket end
-          {
-            protocol: 'https',
-            hostname: 'm.media-amazon.com'
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com'
-          }
-        ]
-    },
     env: {
-
-        API_URL: 'https://readme-backend.fly.dev',
-      },
+      API_URL: 'https://readme-backend.fly.dev',
+    },
 
 };
 
