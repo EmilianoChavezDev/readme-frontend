92fa6aab216bb7a2e34af78060d5299b7b32c2c5
EmilianoChavezDev <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-03-02 13:02:50 -0300

    corrigiendo errores

--- src/app/auth/prueba/page.jsx
+++ src/app/auth/prueba/page.jsx
@@ -1,10 +1,10 @@
-import Layout from "@/components/Layout";
+import NavBar from "@/components/NavBar";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-      <Layout />
+      <NavBar />
     </div>
   );
 };
