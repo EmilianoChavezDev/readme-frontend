4dae39bb8a02a893b4140f5a0469dfe11bd82882
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-03-02 02:56:49 -0300

    Updates useBook

--- src/hooks/useBook.js
+++ src/hooks/useBook.js
@@ -63,8 +63,27 @@ const useBook = () => {
     }
   };
 
+  // Actualizar un libro por su id
+  const updateBook = async (id, data) => {
+    setIsLoading(true); // Inicia el estado de carga
+    try {
+      const res = await axios.put(`${process.env.API_URL}/libros/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return res.data;
+    } catch (error) {
+      setError(true);
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   // Devuelve las funciones para agregar un libro, obtener todos los libros, buscar un libro, el error y el estado de carga
-  return { createBook, getAllBooks, getBookByID, error, isLoading };
+  return { createBook, getAllBooks, getBookByID, updateBook, error, isLoading };
 };
 
 export default useBook;
