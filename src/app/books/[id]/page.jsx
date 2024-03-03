092e5b76b531128dbd7c9c4c59edcf048d58f347
godoygabriel72 <godoygabriel72@gmail.com>
2024-03-03 10:05:53 -0300

    cambios agregados para que funcionen con las convenciones acordadas

---
+++ src/app/books/[id]/page.jsx
@@ -0,0 +1,170 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import toast from 'react-hot-toast'
+import { LuEye } from 'react-icons/lu'
+import { useEffect, useState } from 'react'
+import { PiStarThin } from 'react-icons/pi'
+import { useRouter } from 'next/navigation'
+import { FaRegImage } from 'react-icons/fa6'
+import { PiWarningBold } from 'react-icons/pi'
+import { GoListUnordered } from 'react-icons/go'
+
+import useBook from '@/hooks/useBook'
+import NavBar from '@/components/NavBar'
+import { addNumberFormat } from '@/utils'
+import useReview from '@/hooks/useReview'
+import Modal from '@/components/common/modal'
+import Loader from '@/components/common/loader'
+import ReviewSelector from '@/components/books/reviewSelector'
+import CommentsSection from '@/components/books/commentsSection'
+import { useUser } from '@/contexts/UserProvider'
+
+export default function BookDetails({ params }) {
+
+    const router = useRouter()
+
+    const { userId } = useUser()
+    const { getBookByID, isLoading, error } = useBook()
+    const { createOrUpdateReview, getReviewByUserAndBook } = useReview()
+
+    const [book, setBook] = useState(null)
+    const [resenha, setResenha] = useState(null)
+    const [motivoReporte, setMotivoReporte] = useState('')
+    const [showReportModal, setShowReportModal] = useState(false)
+
+    const fetchBook = async () => {
+        const result = await getBookByID(params.id)
+        setBook(result)
+    }
+
+    const fetchReview = async () => {
+        const result = await getReviewByUserAndBook({ libro_id: book.id, user_id: userId })
+        setResenha(result)
+    }
+
+    const updateReview = async puntuacion => {
+        const result = await createOrUpdateReview({ libro_id: book.id, puntuacion})
+        setResenha(result?.resenha)
+    }
+
+    const reportBoot = async () => {
+        toast.error('Falta implementar en la API')
+        setShowReportModal(false)
+    }
+
+    useEffect(() => {
+        fetchBook()
+    }, [params.id])
+    
+    useEffect(() => {
+        if (userId && book?.id) {
+            fetchReview()
+        }
+    }, [book?.id, userId])
+
+    useEffect(() => {
+        console.log(isLoading)
+    }, [isLoading])
+
+    return (
+        <>
+            <NavBar />
+            <Modal open={Boolean(showReportModal)}
+                onHide={() => setShowReportModal(false)}
+                onSave={reportBoot}
+                title='Denunciar Libro'>
+                <div className='flex flex-col gap-2'>
+                    <span>Indícanos el motivo de tu reporte</span>
+                    <textarea className='text-xs border rounded-lg p-3 flex-grow border-gray-400 outline-none' 
+                        value={motivoReporte} 
+                        onChange={event => setMotivoReporte(event.target.value)} 
+                        rows={2} />
+                </div>
+            </Modal>
+            {error?
+                <div className='flex justify-center items-center'>
+                    <h1>Libro no encontrado</h1>
+                </div> :
+                <div className='flex flex-col gap-3 relative'>
+                    {isLoading && <Loader />}
+                    <section className='flex flex-grow flex-wrap shadow-lg'>
+                        <div className='flex justify-center items-center min-w-96 w-full _lg:w-1/2'>
+                            <div className='flex gap-5 p-10 flex-col _sm:flex-row _sm:p-3 _md:p-12 _xl:p-16'>
+                                <div className='flex justify-center items-center bg-colorPrimario'>
+                                    <div className='flex justify-center items-center w-44 h-42 !min-h-42 text-white'>
+                                        {book?.portada?
+                                            <Image src={book.portada} width={180} height={180} alt='Portada De Libro' priority={true}/> 
+                                                : <FaRegImage size={35}/>
+                                        }
+                                    </div>
+                                </div>
+                                <div className='flex-grow p-3 flex flex-col gap-3'>
+                                    <h1 className='font-extrabold text-xl'>{book?.titulo}</h1>
+                                    <div className='flex'>
+                                        <div className='flex flex-col items-center flex-grow pr-2'>
+                                            <div className='flex gap-1 items-center text-gray-400'>
+                                                <LuEye />
+                                                <span className='text-sm'>Lecturas</span>
+                                            </div>
+                                            <span className='font-semibold'>{addNumberFormat(book?.cantidad_lecturas)}</span>
+                                        </div>
+                                        <div className='flex flex-col items-center flex-grow px-2 border-r border-l border-gray-400'>
+                                            <div className='flex gap-1 items-center text-gray-400'>
+                                                <PiStarThin strokeWidth={10}/>
+                                                <span className='text-sm'>Puntuación</span>
+                                            </div>
+                                            <span className='font-semibold'>{addNumberFormat(Number(book?.puntuacion_media.toFixed(1)))}</span>
+                                        </div>
+                                        <div className='flex flex-col items-center flex-grow pl-2'>
+                                            <div className='flex gap-1 items-center text-gray-400'>
+                                                <GoListUnordered />
+                                                <span className='text-sm'>Partes</span>
+                                            </div>
+                                            <span className='font-semibold'>0</span>
+                                        </div>
+                                    </div>
+                                    <div className='flex flex-col gap-3 text-white text-xs'>
+                                        <Link href={`/libros/${params.id}/leer`}>
+                                            <button className='h-9 rounded-md bg-colorPrimario w-full'>
+                                                Comenzar a Leer
+                                            </button>
+                                        </Link>
+                                        <button className='h-9 rounded-md bg-gray-500' onClick={() => toast.error('Implementar en otro ticket')}>
+                                            Añadir a Favoritos
+                                        </button>
+                                        <button className='h-9 rounded-md bg-gray-500' onClick={() => toast.error('Implementar en otro ticket')}>
+                                            Descargar
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='relative flex justify-center items-center min-w-96 w-full _lg:w-1/2'>
+                            <div className='flex flex-col gap-2 p-9 _lg:p-16'>
+                                <h2 className='font-semibold'>Sinopsis:</h2>
+                                <p className='text-sm'>
+                                    {book?.sinopsis}
+                                </p>
+                            </div>
+                            <button className='absolute bottom-5 right-10 bg-none outline-none border-none text-red-600 flex gap-1' onClick={() => setShowReportModal(true)}>
+                                <span>
+                                    <PiWarningBold />
+                                </span>
+                                <span className='text-xs whitespace-nowrap'>
+                                    Denunciar este libro
+                                </span>
+                            </button>
+                            <div className='absolute top-10 right-10'>
+                                <ReviewSelector currentPoint={resenha?.puntuacion ?? 0} onSelect={updateReview}/>
+                            </div>
+                        </div>
+                    </section>
+                    <CommentsSection bookId={book?.id} />
+                </div>
+            }
+        </>
+    )
+
+}
---
+++ src/components/books/commentsSection.jsx
@@ -0,0 +1,135 @@
+'use client'
+import 'moment/locale/es'
+
+import moment from 'moment'
+import toast from 'react-hot-toast'
+import { useEffect, useState } from 'react'
+import { VscKebabVertical } from 'react-icons/vsc'
+import { Popover, PopoverContent, PopoverHandler } from '@material-tailwind/react'
+
+import useComment from '@/hooks/useComment'
+import Modal from '@/components/common/modal'
+import { useUser } from '@/contexts/UserProvider'
+
+export default function CommentsSection({ bookId }) {
+
+    moment.locale('es')
+    const COMMENTS_PAGE_SIZE = 5
+    const { username } = useUser()
+    const { getCommentsByUserAndBook, deleteComment, createComment } = useComment()
+
+    const [comment, setComment] = useState('')
+    const [comments, setComments] = useState({ list: [] })
+    const [commentsListPage, setCommentsListPage] = useState(1)
+    const [commentIdToRemove, setCommentIdToRemove] = useState(null)
+
+    const fetchComments = async () => {
+        const data = await getCommentsByUserAndBook({ libro_id: bookId, page: commentsListPage, size: COMMENTS_PAGE_SIZE })
+        const { resultado, cant_paginas } = data ?? {}
+        let copiaDeComentarios = {...comments}
+        setComments({ total_pages: cant_paginas, list: [...copiaDeComentarios.list, ...resultado] })
+    }
+
+    const addComment = async () => {
+        await createComment({ libro_id: bookId, comentario: comment })
+        const data = await getCommentsByUserAndBook({ libro_id: bookId, page: 1, size: COMMENTS_PAGE_SIZE })
+        const { resultado, cant_paginas } = data ?? {}
+        setCommentsListPage(1)
+        setComments({ total_pages: cant_paginas, list: resultado })
+        setComment('')
+    }
+
+    const removeComment = async () => {
+        const result = await deleteComment(commentIdToRemove)
+        if (result?.error) {
+            toast.error(result?.error)
+        } else {
+            toast.success('Comentario eliminado correctamente')
+            const indexComentario = comments?.list?.findIndex(comment => comment.id === commentIdToRemove)
+            let listDeComentarios = [...comments.list]
+            listDeComentarios.splice(indexComentario, 1)
+            setComments({...comments, list: listDeComentarios })
+        }
+        setCommentIdToRemove(null)
+    }
+
+    useEffect(() => {
+        bookId && fetchComments()
+    }, [commentsListPage, bookId])
+
+    return (
+        <>
+            <Modal open={Boolean(commentIdToRemove)}
+                onHide={() => setCommentIdToRemove(null)}
+                onSave={removeComment}
+                title='Eliminar Comentario'>
+                <span>Estás seguro de eliminar este comment?</span>
+            </Modal>
+            <section className='flex flex-wrap'>
+                <div className='w-full _lg:w-1/2 px-9 _lg:px-16 min-w-96 py-4 flex flex-col gap-3'>
+                    <span className='font-extrabold text-xl'>Comentarios</span>
+                    <div className='flex gap-2 flex-wrap justify-end'>
+                        <div className='flex gap-2 min-w-80 flex-grow'>
+                            <div className='w-8 h-8 rounded-full bg-colorPrimario text-white flex justify-center items-center'>
+                                {username?.charAt(0)?.toUpperCase()}
+                            </div>
+                            <textarea className='text-xs border rounded-lg p-3 flex-grow border-gray-400 outline-none' 
+                                placeholder='Añadir un comment' 
+                                rows={5}
+                                value={comment}
+                                onChange={event => setComment(event.target.value)}
+                                onFocus={event => event.target.select()}/>
+                        </div>
+                        <button className='bg-colorPrimario text-white h-9 px-3 rounded-lg disabled:cursor-not-allowed' onClick={addComment} disabled={!Boolean(comment?.trim()?.length)}>
+                            Añadir
+                        </button>
+                    </div>
+                </div>
+                <div className='w-full _lg:w-1/2 px-9 _lg:px-16 py-4'>
+                    <ul>
+                        {comments?.list?.map((item, index) =>
+                            <li key={index} className='border-b-[1px] border-gray-300 pb-2'>
+                                <div className='text-xs flex flex-col gap-2'>
+                                    <header className='flex justify-between flex-grow'>
+                                        <div className='flex gap-2 items-center h-10'>
+                                            <div className='w-8 h-8 rounded-full bg-colorPrimario text-white flex justify-center items-center'>
+                                                {item.username.charAt(0).toLocaleUpperCase()}
+                                            </div>
+                                            <span className='text-black font-bold'>{item.username}</span>
+                                            <span className='text-gray-400 whitespace-nowrap'>{moment(item.created_at).startOf('minutes').fromNow()}</span>
+                                        </div>
+                                        {item.username === username &&
+                                            <Popover placement='left'>
+                                                <PopoverHandler>
+                                                    <button className='h-9'>
+                                                        <VscKebabVertical size={15} />
+                                                    </button>
+                                                </PopoverHandler>
+                                                <PopoverContent className='shadow-lg p-1 border-gray-400'>
+                                                    <div className='p-1 text-xs text-black'>
+                                                            <span className='cursor-pointer text-gray-700 hover:text-black' onClick={() => setCommentIdToRemove(item.id)}>
+                                                                Eliminar
+                                                            </span>
+                                                    </div>
+                                                </PopoverContent>
+                                            </Popover>
+                                        }
+                                    </header>
+                                    <p>{item?.comentario}</p>
+                                </div>
+                            </li>
+                        )}
+                    </ul>
+                    <div className='flex justify-center py-1'>
+                        {commentsListPage < comments.total_pages &&
+                            <button className='bg-colorPrimario text-white h-9 rounded-md px-3 text-xs' onClick={() => setCommentsListPage(commentsListPage + 1)}>
+                                Ver más
+                            </button>
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+
+}
