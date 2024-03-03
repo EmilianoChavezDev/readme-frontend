966a732a14b84dab0e99d5790b711dd3efc44b1a
Jorge <jorgemf195@gmail.com>
2024-03-03 02:19:44 -0300

    get y update listos

---
+++ src/app/favorites/page.jsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./styles/favorites.module.css";
+import Cuadros from "./squares.jsx";
+import useFavoritos from "./hooks/useFavorites";
+import { useUser } from "@/contexts/UserProvider";
+
+const PageFavoritos = () => {
+  const [filtro, setFiltro] = useState("");
+  const [librosFavoritos, setLibrosFavoritos] = useState([]);
+  const { traerFavoritosPorUsuario, error, isLoading } = useFavoritos();
+  const { token, userId } = useUser();
+
+  useEffect(() => {
+    const pagina = 1;
+    if (token) {
+      traerFavoritosPorUsuario(userId, pagina, token)
+        .then((favoritos) => {
+          setLibrosFavoritos(favoritos);
+          console.log(favoritos);
+        })
+        .catch(() => {
+          console.error("Error al traer favoritos:", error);
+        });
+    }
+  }, [token]);
+
+  const cuadrosFiltrados = librosFavoritos?.filter(({ titulo, autorUsername
+  }) => {
+    const lowerCaseTitulo = titulo ? titulo.toLowerCase() : "";
+    const lowerCaseAutor = autorUsername ? autorUsername.toLowerCase() : "";
+    const lowerCaseFiltro = filtro.toLowerCase();
+
+    return (
+      lowerCaseTitulo.includes(lowerCaseFiltro) ||
+      lowerCaseAutor.includes(lowerCaseFiltro)
+    );
+  });
+
+  if (isLoading) {
+    return <div> CARGANDO... </div>;
+  }
+
+  return (
+    <div className={styles.contenedor_global}>
+      <div className={styles.contenedor_principal}>
+        <div className={styles.barra_principal}>
+          <div>
+            <h1 className={styles.titulo_favorito}>Mis Favoritos</h1>
+          </div>
+          <div>
+            <div>
+              <input
+                className={styles.buscador}
+                type="text"
+                placeholder="Buscar en Favoritos"
+                value={filtro}
+                onChange={(e) => setFiltro(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.contenedor_padre_cuadros}>
+          <div className={styles.contenedor_cuadros}>
+            {cuadrosFiltrados?.map(({id, titulo, autorUsername, portada,cantidad_lecturas,puntuacion_media, cantidad_comentarios, token, userId}) => (
+              <Cuadros
+                libroId={id}
+                imageUrl={portada}
+                title={titulo}
+                author={autorUsername}
+                view={cantidad_lecturas}
+                star={puntuacion_media}
+                comment={cantidad_comentarios}
+                token={token}
+                userId={userId}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageFavoritos;
---
+++ src/app/favorites/squares.jsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./styles/favorites.module.css";
+import updateFavoritos from "./hooks/updateFavorites";
+import { useUser } from "@/contexts/UserProvider";
+import Link from "next/link";
+
+const Cuadros = ({ libroId, imageurl, title, author, view, star, comment }) => {
+  const [favorito, setFavorito] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const { actualizarFavoritos, error, isLoading } = updateFavoritos();
+  const { token, userId } = useUser();
+
+  const fn_btnFavorite = (clickedLibroId) => {
+    if (token) {
+      setFavorito(!favorito);
+      actualizarFavoritos(clickedLibroId, userId, !favorito, token)
+        .then((favoritos) => {
+          console.log(
+            `Se ha ${
+              favorito ? "agregado a" : "sacado de"
+            } favoritos el libro ${clickedLibroId}`,
+            favoritos
+          );
+        })
+        .catch(() => {
+          console.error("Error al intentar actualizar favoritos:", error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      setLoading(false);
+    }
+  }, [token]);
+
+  if (loading) {
+    return <div> CARGANDO... </div>;
+  }
+
+  return (
+    <Link href={`/libro/${libroId}`}>
+      <div className={styles.contenedor_datos_cuadro}>
+        <div
+          className={styles.botonContainer}
+          onClick={() => fn_btnFavorite(libroId)}
+        >
+          {favorito ? (
+            <button className={styles.btnCorazonLleno}>
+              <img src="/image/img_like.png" alt="Corazón lleno" />
+            </button>
+          ) : (
+            <button className={styles.btnCorazonVacio} onClick={fn_btnFavorite}>
+              <img src="/image/img_dislike.png" alt="Corazón vacio" />
+            </button>
+          )}
+        </div>
+        <div>
+          <img src={imageurl}></img>
+        </div>
+        <div>
+          <p className={styles.title}>{title}</p>
+        </div>
+        <div>
+          <p className={styles.author}>{author}</p>
+        </div>
+        <div className={styles.group_global}>
+          <div className={styles.group_children}>
+            <div>
+              <img src="/image/img_view.png" alt="imagen ver"></img>
+            </div>
+            <div>
+              <p>{view}</p>
+            </div>
+          </div>
+          <div className={styles.group_children}>
+            <div>
+              <img src="/image/img_star.png" alt="imagen estrella"></img>
+            </div>
+            <div>
+              <p>{star}</p>
+            </div>
+          </div>
+          <div className={styles.group_children}>
+            <div>
+              <img src="/image/img_comment.png" alt="imagen comentar"></img>
+            </div>
+            <div>
+              <p>{comment}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Cuadros;
