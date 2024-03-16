f6fc7a578e8062da5434ebdc3bdb46eb93320e48
HamudiEZ <131073895+HamudiEZ@users.noreply.github.com>
2024-03-16 15:02:56 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://127.0.0.1:4000",
+    API_URL: "http://localhost:4000",
   },
   images: {
     remotePatterns: [
