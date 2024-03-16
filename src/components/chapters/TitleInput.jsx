44268e7c8d82b7302690f77c52e6431ce7ae755d
godoygabriel72 <godoygabriel72@gmail.com>
2024-03-16 17:12:15 -0300

    renombrado componente mal escrito

--- src/components/chapters/TitleInput.jsx
+++ src/components/chapters/TitleInput.jsx
@@ -11,11 +11,11 @@ const TittleInput = ({ ...props }) => {
     <div className="flex justify-center w-full">
       <input
         ref={inputRef}
+        placeholder='Ingrese el título del capítulo'
         type="text"
         className="text-2xl font-semibold border-none w-1/2 bg-transparent outline-none text-center hover:bg-gray-100 px-6 py-2 hover:cursor-pointer focus:cursor-text"
         onClick={handleInputClick}
-        {...props}
-      />
+        {...props} />
     </div>
   );
 };
