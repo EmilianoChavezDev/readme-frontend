f09e1982a1a30c8c627ac629f8d9b08d86c2b6e2
Jorg7Mr <jorgemf195@gmail.com>
2024-03-17 14:51:52 -0300

    Cambiando Redireccion a Borradores

--- src/app/books/[id]/chapters/write/page.jsx
+++ src/app/books/[id]/chapters/write/page.jsx
@@ -15,7 +15,7 @@ export default function WriteChapter({ params }) {
         const createdChapter = await createChapter(values)
         if (createdChapter) {
             toast.success('El capítulo de tu libro ha sido guardado')
-            router.push(`/books/${params.id}`)
+            router.push('/drafts')
         } else {
             toast.error('El capítulo de tu libro no se pudo guardar')
         }
