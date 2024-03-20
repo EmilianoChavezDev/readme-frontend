8d45414b056bb2e8b8cffdfca51f6d17917777cf
HamudiEZ <131073895+HamudiEZ@users.noreply.github.com>
2024-03-19 21:34:04 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NavBar from "@/components/NavBar";
 import { useUser } from "@/contexts/UserProvider";
 import useGetLibros from "@/hooks/useGetLibros";
 import useGetLibrosLeidos from "@/hooks/useGetLibrosLeidos";
@@ -9,10 +10,9 @@ import {
   CardHeader,
   Typography,
 } from "@material-tailwind/react";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import NavBar from "@/components/NavBar";
 
 export default function BackgroundBlogCard() {
   const { getLibros, data: libros } = useGetLibros(); // Obtener la función para obtener libros y los datos de libros
@@ -38,62 +38,68 @@ export default function BackgroundBlogCard() {
 
   return (
     <>
-      <NavBar />
+      <div className="sticky top-0 z-50">
+        <NavBar />
+      </div>
       <div className="container-fluid p-12 h-full">
         <div className="mb-32">
-          <h2 className="text-4xl font-semibold mb-4">Seguir Leyendo</h2>
-          <div>
-            <div className="grid gap-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 pb-20 shadow-[0_19px_10px_-19px_rgba(0,0,0,0.3)]">
-              {librosLeidos.slice(0, 6).map((libro) => (
-                <div key={libro.id} className="flex justify-center">
-                  <Link
-                    href={`/books/${libro.id}`}
-                    className="flex justify-center w-auto h-auto"
-                  >
-                    <Card
-                      shadow={false}
-                      className="relative grid w-customWidth h-customHeight items-end justify-center overflow-hidden text-center"
+          {librosLeidos.length > 0 && (
+            <>
+              <h2 className="text-3xl text-center _sm:text-left font-bold mb-4">
+                Seguir Leyendo
+              </h2>
+              <div className="grid gap-8 grid-cols-1 _sm:grid-cols-2 _md:grid-cols-3 _lg:grid-cols-4 _xl:grid-cols-5 pb-20 shadow-[0_19px_10px_-19px_rgba(0,0,0,0.3)]">
+                {librosLeidos.slice(0, 6).map((libro) => (
+                  <div key={libro.id} className="flex justify-center">
+                    <Link
+                      href={`/books/${libro.id}`}
+                      className="flex justify-center w-auto h-auto"
                     >
-                      <CardHeader
-                        floated={false}
+                      <Card
                         shadow={false}
-                        color="transparent"
-                        className="absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center"
+                        className="relative grid w-customWidth h-customHeight items-end justify-center overflow-hidden text-center"
                       >
-                        <Image
-                          src={
-                            libro?.portada.length
-                              ? libro.portada
-                              : "/image/template_libro.png"
-                          }
-                          alt={`Portada del libro "${libro.titulo}"`}
-                          width={250}
-                          height={350}
+                        <CardHeader
+                          floated={false}
+                          shadow={false}
+                          color="transparent"
                           className="absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center"
-                        />
-                        <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
-                      </CardHeader>
-                      <CardBody className="relative py-8 px-6 md:px-8">
-                        <Typography
-                          variant="h5"
-                          color="white"
-                          className="mb-6 font-medium leading-[1.5]"
                         >
-                          {libro.titulo}
-                        </Typography>
-                      </CardBody>
-                    </Card>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
+                          <Image
+                            src={
+                              libro?.portada.length
+                                ? libro.portada
+                                : "/image/template_libro.png"
+                            }
+                            alt={`Portada del libro "${libro.titulo}"`}
+                            width={250}
+                            height={350}
+                            className="absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center"
+                          />
+                          <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
+                        </CardHeader>
+                        <CardBody className="relative py-8 px-6 md:px-8">
+                          <Typography
+                            variant="h5"
+                            color="white"
+                            className="mb-6 font-medium leading-[1.5]"
+                          >
+                            {libro.titulo}
+                          </Typography>
+                        </CardBody>
+                      </Card>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
 
           <div className="my-12"></div>
-          <h2 className="text-4xl font-semibold mb-4 ">Novedades</h2>
+          <h2 className="text-3xl font-bold mb-4 ">Novedades</h2>
 
-          <div className="grid gap-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-            {libros?.data?.slice(0, 6).map((libro) => (
+          <div className="grid gap-8 grid-cols-1 _sm:grid-cols-2 _md:grid-cols-3 _lg:grid-cols-4 _xl:grid-cols-5">
+            {libros?.data?.slice(0, 10).map((libro) => (
               <div key={libro.id} className="flex justify-center">
                 <Link
                   href={`/books/${libro.id}`}
