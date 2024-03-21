ba80a5c4af7108d061a9813478c3335d4dc3a272
EmilianoChavezDev <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-03-21 15:09:15 -0300

    subiendo avances

--- src/app/accounts/edit/[id]/page.jsx
+++ src/app/accounts/edit/[id]/page.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+"use client";
+
+import NavBar from "@/components/NavBar";
 
 const page = ({ params }) => {
   console.log(params);
-  return <div>{params}</div>;
+  return (
+    <>
+      <NavBar />
+      <div className="flex flex-col _sm:w-4/6 w-full">
+        <div>
+          <h1 className="text-textHeaderColorGray">Informacion Personal</h1>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default page;
--- src/app/accounts/page.jsx
+++ src/app/accounts/page.jsx
@@ -5,7 +5,7 @@ import { FaUser, FaChartBar } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-const page = ({ params }) => {
+const page = () => {
   const { username } = useUser();
   const router = useRouter();
   return (
@@ -19,13 +19,11 @@ const page = ({ params }) => {
         </div>
         <div>
           <ul className="flex flex-col _lg:flex-row _lg:justify-around _lg:mx-16 mx-7 gap-y-4 _lg:gap-x-0">
-            <li
-              className="flex items-center gap-x-3 hover:cursor-pointer py-4 px-2 rounded-lg hover:shadow-lg transition-all transform duration-300 hover:scale-105
-            _lg:py-6 _lg:flex-col _lg:text-left _lg:items-start _lg:gap-y-4 _lg:px-4
-            
-            "
-            >
-              <Link href={`/edit/${username}`}>
+            <li className="hover:cursor-pointer rounded-lg hover:shadow-lg transition-all transform duration-300 hover:scale-105">
+              <Link
+                href={`/accounts/edit/${username}`}
+                className="flex items-center gap-x-3  py-4 px-2  _lg:py-6 _lg:flex-col _lg:text-left _lg:items-start _lg:gap-y-4 _lg:px-4"
+              >
                 <div>
                   <FaUser size={20} className="_lg:size-10" />
                 </div>
@@ -41,24 +39,24 @@ const page = ({ params }) => {
               </Link>
             </li>
 
-            <li
-              className="flex items-center gap-x-3 hover:cursor-pointer py-4 px-2 rounded-lg hover:shadow-lg transition-all transform duration-300 hover:scale-105
-            _lg:py-6 _lg:flex-col _lg:text-left _lg:items-start _lg:gap-y-4 _lg:px-4
-            
-            "
-            >
-              <div>
-                <FaChartBar size={20} className="_lg:size-10" />
-              </div>
-              <div>
-                <p className="font-semibold text-textInformationColor">
-                  Estadísticas
-                </p>
-                <span className="text-textColorGray  text-sm">
-                  Obtener un informe de las interacciones de los usuarios con
-                  los libros publicados
-                </span>
-              </div>
+            <li className="hover:cursor-pointer rounded-lg hover:shadow-lg transition-all transform duration-300 hover:scale-105">
+              <Link
+                href={"#"}
+                className="flex items-center gap-x-3  py-4 px-2  _lg:py-6 _lg:flex-col _lg:text-left _lg:items-start _lg:gap-y-4 _lg:px-4"
+              >
+                <div>
+                  <FaChartBar size={20} className="_lg:size-10" />
+                </div>
+                <div>
+                  <p className="font-semibold text-textInformationColor">
+                    Estadísticas
+                  </p>
+                  <span className="text-textColorGray  text-sm">
+                    Obtener un informe de las interacciones de los usuarios con
+                    los libros publicados
+                  </span>
+                </div>
+              </Link>
             </li>
           </ul>
         </div>
