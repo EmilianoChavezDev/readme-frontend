d8b27a69fd3a02fb413fa14613bdcefde67d1e29
Emiliano Miranda <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-03-26 19:58:42 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    API_URL: "http://localhost:4000",
+    API_URL: "https://readme-backend.fly.dev",
   },
   images: {
     remotePatterns: [
