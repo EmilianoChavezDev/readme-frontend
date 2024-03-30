c67829fffce5f4d32f5b0267b9003f5661a27b3e
Jorg7Mr <jorgemf195@gmail.com>
2024-03-30 02:37:04 -0400

    agregando get categorias

--- src/hooks/useBook.js
+++ src/hooks/useBook.js
@@ -1,52 +1,89 @@
-'use client'
+"use client";
 
-import axios from 'axios'
-import { useState } from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 const useBook = () => {
+  const BOOK_ENDPOINT = "/libros";
 
-    const BOOK_ENDPOINT = '/libros'
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [error, setError] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
+  // Create an axios instance with the base URL and token
+  const api = axios.create({ baseURL: process.env.API_URL });
 
-    // Create an axios instance with the base URL and token
-    const api = axios.create({ baseURL: process.env.API_URL })
-
-    const handleRequest = async requestFunction => {
-        setIsLoading(true)
-        try {
-            const res = await requestFunction()
-            return res.data
-        } catch (error) {
-            setError(true)
-        } finally {
-            setIsLoading(false)
-        }
+  const handleRequest = async (requestFunction) => {
+    setIsLoading(true);
+    try {
+      const res = await requestFunction();
+      return res.data;
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    // Functions to interact with the API
-    const createBook = async params => {
-        return handleRequest(() => api.post(BOOK_ENDPOINT, params, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }))
-    }
+  // Functions to interact with the API
+  const createBook = async (params) => {
+    return handleRequest(() =>
+      api.post(BOOK_ENDPOINT, params, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+    );
+  };
 
-    const getAllBooks = async params => {
-        return handleRequest(() => api.get(BOOK_ENDPOINT, { params, headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }))
-    }
+  const getAllBooks = async (params) => {
+    return handleRequest(() =>
+      api.get(BOOK_ENDPOINT, {
+        params,
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+    );
+  };
 
-    const getBookByID = async id => {
-        return handleRequest(() => api.get(`${BOOK_ENDPOINT}/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }))
-    }
+  const getBookByID = async (id) => {
+    return handleRequest(() =>
+      api.get(`${BOOK_ENDPOINT}/${id}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+    );
+  };
 
-    const updateBook = async (id, params) => {
-        return handleRequest(() => api.put(`${BOOK_ENDPOINT}/${id}`, params, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }))
-    }
+  const updateBook = async (id, params) => {
+    return handleRequest(() =>
+      api.put(`${BOOK_ENDPOINT}/${id}`, params, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+    );
+  };
 
-    const deleteBook = async id => {
-        return handleRequest(() => api.delete(`${BOOK_ENDPOINT}/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }))
-    }
+  const deleteBook = async (id) => {
+    return handleRequest(() =>
+      api.delete(`${BOOK_ENDPOINT}/${id}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+    );
+  };
 
-    return { createBook, getAllBooks, getBookByID, updateBook, deleteBook, error, isLoading }
-}
+  const getCategory = async () => {
+    return handleRequest(() =>
+      api.get(`${BOOK_ENDPOINT}_categorias`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+    );
+  };
 
-export default useBook
+  return {
+    createBook,
+    getAllBooks,
+    getBookByID,
+    updateBook,
+    deleteBook,
+    getCategory,
+    error,
+    isLoading,
+  };
+};
+
+export default useBook;
