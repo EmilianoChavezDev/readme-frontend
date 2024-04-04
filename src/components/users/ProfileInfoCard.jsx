622938d3146f6dc6a953c233630645ddc5b11661
EmilianoChavezDev <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-04-04 07:41:08 -0400

    componente card para informacion del usuario

--- src/components/users/ProfileInfoCard.jsx
+++ src/components/users/ProfileInfoCard.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Card, CardBody, Typography } from "@material-tailwind/react";
+import { FaBirthdayCake } from "react-icons/fa";
+import { RiMapPin2Fill } from "react-icons/ri";
+import { AiOutlinePaperClip } from "react-icons/ai";
 
-const ProfileInfoCard = () => {
+export function ProfileInfoCard({
+  direction,
+  birthday,
+  createAt,
+  description,
+}) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Card className="_sm:w-80 _sm:h-96 w-72 rounded-xl shadow-lg">
+      <CardBody>
+        <div className="flex flex-col gap-y-4">
+          <div className="flex items-center gap-2">
+            <RiMapPin2Fill size={18} />
+            <span>{direction}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <FaBirthdayCake size={18} />
+            <span>{birthday}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <span className="font-semibold">Se ha unido</span>
+            {createAt}
+          </div>
+          <div className="flex items-start gap-2">
+            <span className="flex mt-1">
+              <AiOutlinePaperClip size={18} />
+            </span>
+            <span>{description}</span>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
 }
-
-export default ProfileInfoCard
