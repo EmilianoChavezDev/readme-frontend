ab929aeaf6c2ff5e722e1a685bd16e633414c28d
EmilianoChavezDev <119908101+EmilianoChavezDev@users.noreply.github.com>
2024-04-13 11:08:38 -0400

    creando el apartado de editar perfil

--- src/app/user/[id]/page.jsx
+++ src/app/user/[id]/page.jsx
@@ -1,15 +1,26 @@
 "use client";
 
 import MyBooksContainer from "@/components/books/mybooks/BooksContainer";
+import InputField from "@/components/common/InputField";
 import Loader from "@/components/common/loader";
 import SearchItem from "@/components/search/SearchItem";
 import ProfileHeader from "@/components/users/ProfileHeader";
+import ProfileImageUploader from "@/components/users/ProfileImage";
 import { ProfileInfoCard } from "@/components/users/ProfileInfoCard";
 import { UserCard } from "@/components/users/UserCard";
 import UserOption from "@/components/users/UserOption";
 import useBook from "@/hooks/useBook";
 import useUserInfo from "@/hooks/useUser";
+import { Button } from "@material-tailwind/react";
 import React, { useEffect, useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
+import { BsPersonFillGear } from "react-icons/bs";
+import { CiCamera } from "react-icons/ci";
+
+const defaultValues = {
+  username: "",
+  fecha_nacimiento: null,
+};
 
 const page = ({ params }) => {
   const { getUserInformation, data } = useUserInfo();
@@ -22,18 +33,44 @@ const page = ({ params }) => {
   const { getAllBooks } = useBook();
   const [selectedOption, setSelectedOption] = useState("misLibros");
   const [arrBooks, setArrBooks] = useState([]);
-  const [isDeleted, setIsDeleted] = useState(false);
   const [usernameLs, setUsernmeLs] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
+  const [profileImage, setProfileImage] = useState(null);
+  const [changeImage, setChangeImage] = useState(false);
+  const [fileInputKey, setFileInputKey] = useState(Date.now());
+  const [isDeleteProfile, setIsDeleteProfile] = useState(false);
+  const [isNotDisable, setIsNotDisable] = useState(true);
+
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    reset,
+    formState: { errors },
+    watch,
+  } = useForm({ defaultValues });
+
+  const onSubmit = async (formData) => {
+    login(formData);
+  };
 
   useEffect(() => {
     getUserInformation(params.id);
   }, []);
 
   useEffect(() => {
+    if (!isDeleteProfile) return;
+    setIsNotDisable(false);
+    setProfileImage(null);
+    setFileInputKey(Date.now());
+  }, [isDeleteProfile]);
+
+  useEffect(() => {
     if (!data) return;
     getFollowFollowers(data?.id);
     getUserLecturas(1, data?.id);
     getAllUserBooks(data?.id);
+    setProfileImage(data?.profile);
     setUsernmeLs(localStorage.getItem("username"));
   }, [data]);
 
@@ -58,8 +95,45 @@ const page = ({ params }) => {
     return usernameLs !== data?.username;
   });
 
-  const hola = () => {
-    console.log("hola mundo");
+  const handleEditChange = () => {
+    setIsEdit(!isEdit);
+    reset({
+      nombre: data?.nombre,
+      direccion: data?.direccion,
+      nacionalidad: data?.nacionalidad,
+      fecha_nacimiento: data?.fecha_de_nacimiento,
+      descripcion: data?.descripcion,
+    });
+  };
+
+  const handleImageChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      const fileExtension = selectedFile.name.split(".").pop().toLowerCase();
+
+      if (
+        fileExtension !== "png" &&
+        fileExtension !== "jpg" &&
+        fileExtension !== "jpeg"
+      ) {
+        toast.error("Solo se permiten archivos PNG, JPG o JPEG.");
+        return;
+      }
+      const reader = new FileReader();
+      setIsDeleteProfile(false);
+      setChangeImage(true);
+      reader.onload = (e) => {
+        setProfileImage(e.target.result);
+      };
+      reader.readAsDataURL(selectedFile);
+      setIsNotDisable(false);
+    }
+  };
+
+  const handleDeleteProfile = () => {
+    setFileInputKey(Date.now());
+    setIsDeleteProfile(true);
+    setChangeImage(false);
   };
 
   return (
@@ -68,18 +142,102 @@ const page = ({ params }) => {
         <Loader />
       ) : (
         <div className="flex flex-col min-h-screen bg-gray-100">
+          {isEdit && (
+            <div className="w-4/6 mx-auto flex justify-between items-center py-2">
+              <div>Estas editando tu perfil ahora</div>
+              <div className="flex gap-2">
+                <Button
+                  className="px-2 py-2 flex text-black border border-colorPrimario bg-white hover:bg-colorHoverPrimario hover:text-white"
+                  onClick={handleEditChange}
+                >
+                  <span className="flex items-center">Guardar Cambios</span>
+                </Button>
+                <Button
+                  className="px-2 py-2 flex text-black border border-textColorGray bg-white hover:bg-textHeaderColorGray hover:text-white"
+                  onClick={handleEditChange}
+                >
+                  <span className="flex items-center">Cancelar</span>
+                </Button>
+              </div>
+            </div>
+          )}
           <div>
-            <ProfileHeader
-              name={data?.nombre}
-              username={data?.username}
-              profile={data?.profile}
-              portada={data?.portada}
-              followers={seguidoresSeguidos?.seguidores}
-              follows={seguidoresSeguidos?.seguidos}
-              read={lecturas?.total_items}
-            />
+            {isEdit ? (
+              <div
+                className={`${
+                  isEdit ? "opacity-100" : "opacity-0"
+                } flex flex-col justify-center items-center transition-all duration-200 transform`}
+              >
+                <div
+                  className="w-full h-full bg-cover bg-center"
+                  style={{
+                    backgroundImage: data?.portada
+                      ? `url(${data?.portada})`
+                      : "linear-gradient(to top, rgba(0, 0, 0, 0.4), transparent)",
+                  }}
+                >
+                  <div>
+                    <label htmlFor="profile-input" className="cursor-pointer">
+                      <div className="inline-block">
+                        <span className="rounded-lg mt-2 ml-2 px-2 py-2 flex items-center gap-1 text-black border border-colorPrimario bg-white hover:bg-colorHoverPrimario hover:text-white">
+                          <CiCamera size={18} />
+                          <span>Actualizar foto de portada</span>
+                        </span>
+                      </div>
+                    </label>
+                    <input
+                      type="file"
+                      id="profile-input"
+                      style={{ display: "none" }}
+                      accept="image/*"
+                      onChange={handleImageChange}
+                    />
+                  </div>
+
+                  <div
+                    className={`flex justify-center items-center h-full  ${
+                      isDeleteProfile && "mb-11"
+                    }`}
+                  >
+                    <ProfileImageUploader
+                      username={data?.username}
+                      profileImage={profileImage}
+                      handleImageChange={handleImageChange}
+                      handleDeleteProfile={handleDeleteProfile}
+                      isDeleteProfile={isDeleteProfile}
+                      setProfileImage={setProfileImage}
+                      key={fileInputKey}
+                    />
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <ProfileHeader
+                name={data?.nombre}
+                username={data?.username}
+                profile={data?.profile}
+                portada={data?.portada}
+                followers={seguidoresSeguidos?.seguidores}
+                follows={seguidoresSeguidos?.seguidos}
+                read={lecturas?.total_items}
+                buttonProps={{
+                  info: (
+                    <div className="flex items-center gap-x-1">
+                      {" "}
+                      <BsPersonFillGear size={18} />
+                      <span>Editar Perfil</span>
+                    </div>
+                  ),
+                  onClick: handleEditChange,
+                }}
+              />
+            )}
           </div>
-          <div className="bg-white">
+          <div
+            className={`bg-white ${
+              isEdit ? "pointer-events-none opacity-50" : ""
+            }`}
+          >
             <UserOption
               isFollow={false}
               selectedOption={selectedOption}
@@ -89,7 +247,7 @@ const page = ({ params }) => {
           </div>
 
           <div className="flex mx-auto w-5/6 mt-4 gap-x-4">
-            <div>
+            <div className={`${isEdit ? "hidden" : "block"}`}>
               <ProfileInfoCard
                 direction={data?.direccion}
                 nacionalidad={data?.nacionalidad}
@@ -98,7 +256,72 @@ const page = ({ params }) => {
                 description={data?.descripcion}
               />
             </div>
-            <div className="flex flex-col w-full h-full rounded-xl p-6 bg-white shadow-lg ml-10">
+            {isEdit && (
+              <div className="flex flex-col w-1/2 mx-auto items-center justify-center min-h-full rounded-lg p-6 bg-white shadow-lg gap-y-4">
+                <span className="w-4/6 mx-auto text-sm mb-4">
+                  La información que introduzcas aquí, incluyendo tu nombre de
+                  usuario, foto de perfil e imagen de fondo, serán visibles a
+                  otros usuarios. Ten cuidado al momento de publicar información
+                  valiosa.
+                </span>
+                <div>
+                  <InputField
+                    label={"Nombre"}
+                    type={"text"}
+                    onBlur={() => trigger("nombre")}
+                    register={register}
+                    name={"nombre"}
+                    required={false}
+                  />
+                </div>
+                <div>
+                  <InputField
+                    label={"Direccion"}
+                    type={"text"}
+                    onBlur={() => trigger("direccion")}
+                    register={register}
+                    name={"direccion"}
+                    required={false}
+                  />
+                </div>
+                <div>
+                  <InputField
+                    label={"Nacionalidad"}
+                    type={"text"}
+                    onBlur={() => trigger("nacionalidad")}
+                    register={register}
+                    name={"nacionalidad"}
+                    required={false}
+                  />
+                </div>
+                <div>
+                  <InputField
+                    label={"Fecha de nacimiento"}
+                    type={"date"}
+                    onBlur={() => trigger("fecha_nacimiento")}
+                    register={register}
+                    name={"fecha_nacimiento"}
+                    required={true}
+                  />
+                </div>
+                <div className="h-auto">
+                  <InputField
+                    label={"Descripción"}
+                    type={"text"}
+                    onBlur={() => trigger("descripcion")}
+                    register={register}
+                    name={"descripcion"}
+                    required={false}
+                    className="h-64"
+                  />
+                </div>
+              </div>
+            )}
+            <div
+              className={`${
+                isEdit ? "hidden" : "block"
+              } flex flex-col w-full h-full rounded-xl p-6 bg-white shadow-lg ml-10`}
+            >
               {selectedOption === "misLibros" && (
                 <div>
                   <div className="flex flex-col">
@@ -149,35 +372,35 @@ const page = ({ params }) => {
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                 </p>
               )}
@@ -188,35 +411,38 @@ const page = ({ params }) => {
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "seguir", onClick: hola }}
+                    buttonProps={{ info: "seguir", onClick: handleEditChange }}
                   />
                   <UserCard
                     username={data?.username}
                     nombre={data?.nombre}
                     image={data?.profile}
                     description={data?.descripcion}
-                    buttonProps={{ info: "eliminar", onClick: hola }}
+                    buttonProps={{
+                      info: "eliminar",
+                      onClick: handleEditChange,
+                    }}
                   />
                 </p>
               )}
