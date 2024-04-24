9b106fb4d45a2bf8ceb420a8619dc417a8c7bcb1
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-04-24 16:20:58 -0400

    Starting design

---
+++ src/app/accounts/setup/page.jsx
@@ -0,0 +1,103 @@
+"use client";
+import InputField from "@/components/common/InputField";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Image from "next/image";
+
+const defaultValues = {
+  username: "",
+  lastname: "",
+  direction: "",
+  nationality: "",
+  description: "",
+};
+
+export default function SetUp() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues });
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  return (
+    <div>
+      <div>
+        <Image
+          src="/image/img_inicio.png"
+          width={400}
+          height={200}
+          alt="imagen presentacion"
+        />
+      </div>
+
+      <div className="bg-colorFondo p-3 rounded-2xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/5 h-3/5 rounded-xl">
+        <div className="flex justify-center py-2">
+          <Image
+            src={"/image/g2.png"}
+            alt="imagen logo"
+            width={250}
+            height={250}
+          />
+        </div>
+
+        <div className="text-center font-bold text-gray-700 pb-5">
+          <p>Para obtener la mejor experiencia ReadMe,</p>
+          <p>¡Cuéntanos sobre ti!</p>
+        </div>
+
+        <div className="flex flex-col gap-y-4 items-center">
+          <InputField
+            label={"Nombre"}
+            type={"text"}
+            onBlur={handleBlur}
+            register={register}
+            name={"username"}
+            className={`bg-white rounded-full`}
+          />
+
+          <InputField
+            label={"Apellido"}
+            type={"text"}
+            onBlur={handleBlur}
+            register={register}
+            name={"lastname"}
+            className={`bg-white rounded-full`}
+          />
+
+          <InputField
+            label={"Direccion"}
+            type={"text"}
+            onBlur={handleBlur}
+            register={register}
+            name={"direction"}
+            className={`bg-white rounded-full`}
+          />
+
+          <InputField
+            label={"Nacionalidad"}
+            type={"text"}
+            onBlur={handleBlur}
+            register={register}
+            name={"nationality"}
+            className={`bg-white rounded-full`}
+          />
+
+          <InputField
+            label={"Descripcion"}
+            type={"text"}
+            onBlur={handleBlur}
+            register={register}
+            name={"description"}
+            className={`bg-white rounded-full`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
