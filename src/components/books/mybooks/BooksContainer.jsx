bb9297177214d836e1f7740b2d43ca0d40cd932c
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-04-29 19:37:07 -0400

    Solucionando errores

--- src/components/books/mybooks/BooksContainer.jsx
+++ src/components/books/mybooks/BooksContainer.jsx
@@ -39,7 +39,7 @@ const MyBooksContainer = ({
 
   return (
     <>
-      <div className={styles.container_drafts}>
+      <div id="book-container" className={styles.container_drafts}>
         <Link href={`/books/${libroId}`}>
           <div className={styles.image_book_container}>
             <Image
@@ -122,6 +122,7 @@ const MyBooksContainer = ({
             </div>
             <div>
               <Link
+                id="continue-writing"
                 className={styles.btn_continue_writting}
                 href={`/books/${libroId}/chapters/write`}
               >
