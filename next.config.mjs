f2d14862645584d9a2871cf47e990982b86a3b7c
Alexis Sauer <=>
2024-04-29 17:36:02 -0400

    Agregado headers a la app

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,39 @@ const nextConfig = {
       },
     ],
   },
-
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "accelerometer=(), camera=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
 
