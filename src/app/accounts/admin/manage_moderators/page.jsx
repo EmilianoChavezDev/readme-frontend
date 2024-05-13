56ca7d984d872127279f55094df2a3689e4e2cac
godoygabriel72 <godoygabriel72@gmail.com>
2024-05-12 23:46:35 -0400

    agregado de creacion de moderadores

--- src/app/accounts/admin/manage_moderators/page.jsx
+++ src/app/accounts/admin/manage_moderators/page.jsx
@@ -1,16 +1,41 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
+import useReport from '@/hooks/useReport'
 import { useAccountContext } from '@/contexts/AccountProvider'
+import AddModeratorModal from '@/components/accounts/addModeratorModal'
 
 export default function Page() {
 
-    const { showAddModeratorModal } = useAccountContext()
+    const [currentPage, setCurrentPage] = useState(1)
+
+    const { getModeratorStatistics } = useReport()
+    const { showAddModeratorModal, setShowAddModeratorModal } = useAccountContext()
+
+    const fetchModerators = async () => {
+        let params = { page: currentPage}
+        const result = await getModeratorStatistics(params)
+        console.log(result)
+    }
+
+    const reloadData = () => {
+        currentPage === 1? fetchModerators() : setCurrentPage(1)
+    }
+
+    useEffect(() => {
+        fetchModerators()
+    }, [currentPage])
 
     return (
-        <div className='flex flex-col gap-2'>
-            <h1>Admin de Moderadores</h1>
-            <span>{showAddModeratorModal && 'Boton activado'}</span>
-        </div>
+        <>
+            <AddModeratorModal show={showAddModeratorModal} 
+                onHide={() => setShowAddModeratorModal(false)}
+                onSave={reloadData} />
+            <div className='flex flex-col gap-2'>
+                <h1>Admin de Moderadores</h1>
+            </div>
+        </>
     )
 
 }
