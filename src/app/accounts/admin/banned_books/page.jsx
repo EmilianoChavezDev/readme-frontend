2125a5343e255717b07f049e81e6d8ed67d0df2c
Jorge Candia <jorcandia03@gmail.com>
2024-05-18 19:28:20 -0400

    corregir error de usuario no autorizado en la pagina admin

--- src/app/accounts/admin/banned_books/page.jsx
+++ src/app/accounts/admin/banned_books/page.jsx
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import { useEffect, useState } from "react";
 import Modal from "@/components/common/modal";
 import Loader from "@/components/common/loader";
+import { useRouter } from "next/navigation";
 import Pagination from "@/components/common/Pagination";
 import { Tooltip, Option, Select, Input } from "@material-tailwind/react";
 import useContentAppeal from "@/hooks/useContentAppeal";
@@ -16,7 +17,7 @@ export default function Page() {
     aceptado: { key: "aceptado", value: "Aceptado" },
     rechazado: { key: "rechazado", value: "Rechazado" },
   };
-
+  const router = useRouter();
   const {
     getAllAppeals,
     postAcceptAppeal,
@@ -147,6 +148,14 @@ export default function Page() {
       isMounted = false;
     };
   }, [error, errorResponse, isLoading, isSuccess]);
+
+  let userRole = localStorage.getItem("role");
+  useEffect(() => {
+    if (userRole === "usuario") {
+      toast.error("Usuario no autorizado");
+      router.push("/");
+    }
+  }, []);
 
   return (
     <>
